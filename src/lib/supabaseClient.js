b9d17d66e2b56d989fff43dd2ec4763612bf7028
Thadd Eldredge <thadd@ese-llc.com>
2026-05-25 16:56:44 -0400

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storageKey: 'sb-ayktuzidcoolddlphqia-auth-token',
@@ -11,3 +9,11 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     detectSessionInUrl: true,
   }
 });
+(async () => {
+  const params = new URLSearchParams(window.location.search);
+  const token  = params.get('token');
+  if (token) {
+    await supabase.auth.setSession({ access_token: token, refresh_token: '' });
+    window.history.replaceState({}, '', window.location.pathname);
+  }
+})();
